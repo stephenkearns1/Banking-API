f7cbb59534cfc2dd8f18330eecb973fb389e942e
stephenkearns1 <bohsbabycrew1@gmail.com>
2017-12-05 16:54:36 +0000

    Restructure Project (#1)
    * Restructured project layout

---
+++ src/main/java/com/mycompany/exceptions/NotFoundException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exceptions;
+
+/**
+ *
+ * @author Stephen
+ */
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class NotFoundException extends WebApplicationException {
+     public NotFoundException(String message) {
+         super(Response.status(Response.Status.NOT_FOUND)
+             .entity(message).type(MediaType.TEXT_PLAIN).build());
+     }
+}
