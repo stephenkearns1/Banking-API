aeb3339ae30dfac6e3f4296d2d59a775f866c9a0
barry lougheed <barrylougheed43@hotmail.com>
2017-12-05 16:27:55 +0000

    Setup Project

---
+++ src/main/java/com/mycompany/bankingservice/Account.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bankingservice;
+
+/**
+ *
+ * @author barry
+ */
+public class Account {
+    
+   private int sortCode;
+   private int accountNum;
+   private double balance;
+   
+   public Account(int sortCode, int accountNum, double balance) {
+        this.sortCode = sortCode;
+        this.accountNum = accountNum;
+        this.balance = balance;
+    }
+
+    public void setSortCode(int sortCode) {
+        this.sortCode = sortCode;
+    }
+
+    public void setAccountNum(int accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+   
+
+    public int getSortCode() {
+        return sortCode;
+    }
+
+    public int getAccountNum() {
+        return accountNum;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+   
+
+    
+   
+    
+}
---
+++ src/main/java/com/mycompany/bankingservice/Customer.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bankingservice;
+
+/**
+ *
+ * @author barry
+ */
+public class Customer {
+
+private String Fname,Sname,email,address,password,securityQ,securityAns;
+private int pin;
+
+    public Customer(String Fname, String Sname, String email, String address, String password, String securityQ, String securityAns, int pin) {
+        this.Fname = Fname;
+        this.Sname = Sname;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+        this.securityQ = securityQ;
+        this.securityAns = securityAns;
+        this.pin = pin;
+    }
+
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getSname() {
+        return Sname;
+    }
+
+    public void setSname(String Sname) {
+        this.Sname = Sname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecurityQ() {
+        return securityQ;
+    }
+
+    public void setSecurityQ(String securityQ) {
+        this.securityQ = securityQ;
+    }
+
+    public String getSecurityAns() {
+        return securityAns;
+    }
+
+    public void setSecurityAns(String securityAns) {
+        this.securityAns = securityAns;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+
+    
+}
