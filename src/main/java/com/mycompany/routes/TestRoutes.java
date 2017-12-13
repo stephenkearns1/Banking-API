9814bbca26d423e42d54b7b2457bc42966a1793d
stephenkearns1 <bohsbabycrew1@gmail.com>
2017-12-13 22:31:24 +0000

    test

---
+++ src/main/java/com/mycompany/routes/TestRoutes.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.routes;
+
+import com.mycompany.exceptions.NotFoundException;
+import com.mycompany.models.Account;
+import com.mycompany.services.AccountService;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Stephen Kearns
+ */
+
+@Path("/test")
+public class TestRoutes {
+    AccountService service;
+    public TestRoutes(){
+        service = new AccountService();
+    }
+    
+    @GET
+    @Path("/hello")
+    public Response Test() {
+        return Response.status(Response.Status.OK).entity("Hello").build();
+    }
+
+    @POST
+    @Path("{cust_id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response createAccount(@PathParam("cust_id") int cust_id) {
+        service.CreateAccount(cust_id);
+        return Response.status(Response.Status.CREATED).entity("Fuck you").build();
+    }
+    
+    @GET
+    @Path("/{accountId}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getAccount(@PathParam("accountId") int id) {
+        Account account = service.getAccount(id);
+        if (account != null) {
+            return Response.status(Response.Status.OK).entity(account).build();
+        } else {
+            throw new NotFoundException("Account not Found");
+        }
+    }
+
+
+}
